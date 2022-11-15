31d6971a6d6f3a6f06bed7f40fb2294d854681f8
CodetPriyanka <103997071+CodetPriyanka@users.noreply.github.com>
2022-11-15 18:24:37 +0000

    Update LocationTestSteps.cs
    I have made changes to TestSTeps where I have used RestSharp and its classes and features to implement the API Get call and test the response code back.

--- TechnicalTest/StepDefintion/LocationTestSteps.cs
+++ TechnicalTest/StepDefintion/LocationTestSteps.cs
@@ -1,6 +1,10 @@
 ﻿using NUnit.Framework;
 using System;
 using TechTalk.SpecFlow;
+using RestSharp;
+using RestSharp.Authenticators;
+using NUnit.Framework;
+using System.Net;
 
 namespace TechnicalTest
 {
@@ -16,12 +20,27 @@ namespace TechnicalTest
         [Given(@"I make a request to get location information (.*),(.*)")]
         public void GivenIMakeARequestToGetLocationInformation(string countryCode, string postCode)
         {
-            
+            //Call API client
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            // Endpoint URL
+            RestRequest request = new RestRequest("http://api.zippopotam.us/"+countryCode +"/"+postCode, Method.Get);
+            // Execute request and store response
+            response = client.Execute(request);
+
         }
         
         [Then(@"I verify the request status (.*)")]
         public void ThenTheRequestShouldBeSuccessful(string isSuccessful)
-        {           
+        {
+            //Assert as per isSuccessful value
+            if(isSuccessful == "true")
+            {
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            }
+            else
+            {
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            }
 
         }
     }
